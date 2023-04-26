a3b0a6a6c2d75a5811755e7da6ba44c7760a6cb5
ChenXingguo <xingguochen@alumni.stanford.edu>
2023-04-25 21:28:50 -0700

    MB4-48: Add simple ORCID login.

--- src/views/users/UserLogin.vue
+++ src/views/users/UserLogin.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { reactive } from 'vue'
-import router from '../../router'
 import { useAuthStore } from '@/stores/AuthStore.js'
+import { ref } from 'vue';
+import axios from 'axios'
+import router from '../../router'
 
 const authStore = useAuthStore()
 
@@ -13,6 +15,16 @@ const submitForm = async () => {
     router.push({ path: '/myprojects' })
   }
 }
+
+const orcidLoginUrl = ref(null)
+
+axios.get(`${import.meta.env.VITE_API_URL}/auth/get-orcid-login-url`)
+  .then(response => {
+    orcidLoginUrl.value = response.data.url;
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
 </script>
 
 <template>
@@ -20,7 +32,7 @@ const submitForm = async () => {
     <form @submit.prevent="submitForm()">
       <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-      <div class="form-floating">
+      <div class="form-floating margin-s-top">
         <input
           v-model.trim="state.email"
           type="text"
@@ -30,7 +42,7 @@ const submitForm = async () => {
         />
         <label for="email">Email address</label>
       </div>
-      <div class="form-floating">
+      <div class="form-floating margin-s-top">
         <input
           v-model.trim="state.password"
           type="password"
@@ -40,8 +52,11 @@ const submitForm = async () => {
         />
         <label for="password">Password</label>
       </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">
+      <button class="w-100 btn btn-lg btn-primary margin-s-top" type="submit">
         Sign in
+      </button>
+      <button class="w-100 btn btn-lg btn-primary btn-white margin-s-top">
+        <a :href="orcidLoginUrl"><img alt="ORCID logo" src="/ORCIDiD_iconvector.svg" class="orcid-icon"/>  Sign in with ORCID</a>
       </button>
 
       <div
@@ -61,5 +76,19 @@ const submitForm = async () => {
   max-width: 330px;
   padding: 15px;
   margin: auto;
+}
+.btn-white {
+  background-color: #ffffff !important;
+  border: 1px solid #ced4da !important;
+}
+.btn-white > a {
+  color: #333333;
+}
+.margin-s-top {
+  margin-top: 10px;
+}
+.orcid-icon {
+  width: 24px;
+  height: 24px;
 }
 </style>
