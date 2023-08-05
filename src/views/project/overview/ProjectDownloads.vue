b46288c63ce6d0fe01cd6465b511f3187ed21613
ChenXingguo <xingguochen@alumni.stanford.edu>
2023-08-05 11:49:51 -0700

    MB4-62: Add project views and download toggle.

--- src/views/project/overview/ProjectDownloads.vue
+++ src/views/project/overview/ProjectDownloads.vue
@@ -1,6 +1,19 @@
 <script setup>
+import { reactive } from 'vue'
 import { usePublicProjectDetailsStore } from '@/stores/PublicProjectDetailsStore.js'
 const projectStore = usePublicProjectDetailsStore()
+const folded = reactive({ 
+  mediaDownloads: true,
+  documentDownloads: true,
+  matrixDownloads: true
+ })
+
+// Define the toggleFold method
+const toggleFold = (section) => {
+  folded[section] = !folded[section]
+}
+
+const foldThreshold = 20
 </script>
 
 <template>
@@ -22,62 +35,63 @@ const projectStore = usePublicProjectDetailsStore()
           <td></td>
         </tr>
         <tr>
-          <th scope="row">Matrix downloads</th>
-          <td>{{ projectStore.overview.project_downloads['X'] }}</td>
-          <td>
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >Matrix landing page (9796 views)</span
-            >
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">Document downloads</th>
-          <td>{{ projectStore.overview.project_downloads['D'] }}</td>
-          <td>
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >Matrix landing page (9796 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >Matrix landing page (9796 views)</span
-            >
-          </td>
-        </tr>
-        <tr>
           <th scope="row">Media downloads</th>
           <td>{{ projectStore.overview.project_downloads['M'] }}</td>
           <td>
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M130184 (2 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            ><span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
+            <div v-if="projectStore.overview.project_downloads.details['M']">
+              <span v-for="(item, index) in projectStore.overview.project_downloads.details['M']" 
+                :class="{'badge': true, 'rounded-pill': true, 'bg-secondary': true, 'spaced-span': true, 'folded': index > foldThreshold && folded.mediaDownloads}"
+              >{{ item.name }} ({{ item.val }})</span>
+              <button v-if="projectStore.overview.project_downloads.details['M'].length > foldThreshold" class="btn btn-link" @click="toggleFold('mediaDownloads')">{{ folded.mediaDownloads ? 'Show more' : 'Show less'  }}</button>
+            </div>
           </td>
         </tr>
-
+        <tr>
+          <th scope="row">Matrix downloads</th>
+          <td>{{ projectStore.overview.project_downloads['X'] }}</td>
+          <td>
+            <td>
+            <div v-if="projectStore.overview.project_downloads.details['X']">
+              <span v-for="(item, index) in projectStore.overview.project_downloads.details['X']" 
+                :class="{'badge': true, 'rounded-pill': true, 'bg-secondary': true, 'spaced-span': true, 'folded': index > foldThreshold && folded.matrixDownloads}"
+              >{{ item.name }} ({{ item.val }})</span>
+              <button v-if="projectStore.overview.project_downloads.details['X'].length > foldThreshold" class="btn btn-link" @click="toggleFold('matrixDownloads')">{{ folded.matrixDownloads ? 'Show more' : 'Show less'  }}</button>
+            </div>
+          </td>
+          </td>
+        </tr>
         <tr>
           <th scope="row">Project downloads</th>
           <td>{{ projectStore.overview.project_downloads['P'] }}</td>
           <td></td>
         </tr>
+        <tr>
+          <th scope="row">Document downloads</th>
+          <td>{{ projectStore.overview.project_downloads['D'] }}</td>
+          <td>
+            <div v-if="projectStore.overview.project_downloads.details['D']">
+              <span v-for="(item, index) in projectStore.overview.project_downloads.details['D']" 
+                :class="{'badge': true, 'rounded-pill': true, 'bg-secondary': true, 'spaced-span': true, 'folded': index > foldThreshold && folded.documentDownloads}"
+              >{{ item.name }} ({{ item.val }})</span>
+              <button v-if="projectStore.overview.project_downloads.details['D'].length > foldThreshold" class="btn btn-link" @click="toggleFold('documentDownloads')">{{ folded.documentDownloads ? 'Show more' : 'Show less'  }}</button>
+            </div>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
 </template>
+
+<style>
+.spaced-span {
+  margin-right: 5px
+}
+
+.spaced-span.folded {
+display: none;
+}
+
+.btn-link {
+padding: 0;
+}
+</style>
--- src/views/project/overview/ProjectViews.vue
+++ src/views/project/overview/ProjectViews.vue
@@ -1,6 +1,19 @@
 <script setup>
+import { reactive } from 'vue'
 import { usePublicProjectDetailsStore } from '@/stores/PublicProjectDetailsStore.js'
 const projectStore = usePublicProjectDetailsStore()
+const folded = reactive({ 
+  mediaViews: true,
+  folioViews: true,
+  matrixViews: true
+ })
+
+// Define the toggleFold method
+const toggleFold = (section) => {
+  folded[section] = !folded[section]
+}
+
+const foldThreshold = 20
 </script>
 
 <template>
@@ -26,74 +39,82 @@ const projectStore = usePublicProjectDetailsStore()
           <td>{{ projectStore.overview.project_views['P'] }}</td>
           <td></td>
         </tr>
-        <tr>
-          <th scope="row">Matrix views</th>
-          <td>{{ projectStore.overview.project_views['X'] }}</td>
-          <td>
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >Matrix landing page (9796 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >Matrix landing page (9796 views)</span
-            >
-          </td>
-        </tr>
-        <tr>
+        <tr v-if="projectStore.overview.project_views['M']">
           <th scope="row">Media views</th>
           <td>{{ projectStore.overview.project_views['M'] }}</td>
           <td>
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M130184 (2 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            ><span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
-            <span class="badge rounded-pill bg-secondary mx-2"
-              >M135220 (3 views)</span
-            >
+            <div v-if="projectStore.overview.project_views.details['M']">
+              <span v-for="(item, index) in projectStore.overview.project_views.details['M']" 
+                :class="{'badge': true, 'rounded-pill': true, 'bg-secondary': true, 'spaced-span': true, 'folded': index > foldThreshold && folded.mediaViews}"
+              >{{ item.name }} ({{ item.val }})</span>
+              <button v-if="projectStore.overview.project_views.details['M'].length > foldThreshold" class="btn btn-link" @click="toggleFold('mediaViews')">{{ folded.mediaViews ? 'Show more' : 'Show less'  }}</button>
+            </div>
           </td>
         </tr>
-
-        <tr>
-          <th scope="row">Views for media list</th>
-          <td>{{ projectStore.overview.project_views['V'] }}</td>
-          <td></td>
-        </tr>
-        <tr>
-          <th scope="row">Specimen list</th>
-          <td>{{ projectStore.overview.project_views['S'] }}</td>
-          <td></td>
-        </tr>
-        <tr>
+        <tr v-if="projectStore.overview.project_views['T']">
           <th scope="row">Taxon list</th>
           <td>{{ projectStore.overview.project_views['T'] }}</td>
           <td></td>
         </tr>
-        <tr>
+        <tr v-if="projectStore.overview.project_views['S']">
+          <th scope="row">Specimen list</th>
+          <td>{{ projectStore.overview.project_views['S'] }}</td>
+          <td></td>
+        </tr>
+        <tr v-if="projectStore.overview.project_views['F']">
+          <th scope="row">Folio views</th>
+          <td>{{ projectStore.overview.project_views['F'] }}</td>
+          <td>
+            <div v-if="projectStore.overview.project_views.details['F']">
+              <span v-for="(item, index) in projectStore.overview.project_views.details['F']" 
+                :class="{'badge': true, 'rounded-pill': true, 'bg-secondary': true, 'spaced-span': true, 'folded': index > 20 && folded.folioViews}"
+              >{{ item.name }} ({{ item.val }})</span>
+              <button v-if="projectStore.overview.project_views.details['F'].length > foldThreshold" class="btn btn-link" @click="toggleFold('folioViews')">{{ folded.folioViews ? 'Show more' : 'Show less' }}</button>
+            </div>
+          </td>
+        </tr>
+        <tr v-if="projectStore.overview.project_views['V']">
+          <th scope="row">Views for media list</th>
+          <td>{{ projectStore.overview.project_views['V'] }}</td>
+          <td></td>
+        </tr>
+        <tr v-if="projectStore.overview.project_views['B']">
           <th scope="row">Bibliography</th>
           <td>{{ projectStore.overview.project_views['B'] }}</td>
           <td></td>
         </tr>
-        <tr>
+        <tr v-if="projectStore.overview.project_views['D']">
           <th scope="row">Documents list</th>
           <td>{{ projectStore.overview.project_views['D'] }}</td>
           <td></td>
+        </tr>
+        <tr v-if="projectStore.overview.project_views['X']">
+          <th scope="row">Matrix views</th>
+          <td>{{ projectStore.overview.project_views['X'] }}</td>
+          <td>
+            <div v-if="projectStore.overview.project_views.details['X']">
+              <span v-for="(item, index) in projectStore.overview.project_views.details['X']" 
+                :class="{'badge': true, 'rounded-pill': true, 'bg-secondary': true, 'spaced-span': true, 'folded': index > 20 && folded.matrixViews}"
+              >{{ item.name }} ({{ item.val }})</span>
+              <button v-if="projectStore.overview.project_views.details['X'].length > foldThreshold" class="btn btn-link" @click="toggleFold('matrixViews')">{{ folded.matrixViews ? 'Show more' : 'Show less' }}</button>
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
+
+<style>
+.spaced-span {
+  margin-right: 5px
+}
+
+.spaced-span.folded {
+display: none;
+}
+
+.btn-link {
+padding: 0;
+}
+</style>
