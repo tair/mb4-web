9b287ecf245c54379f2e07d5f1eee4b0b5fe1733
Kenzley Alphonse <kenzley@users.noreply.github.com>
2023-09-24 22:14:17 -0400

    add files

---
+++ src/views/RootView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from '@/components/main/Header.vue'
+import Footer from '@/components/main/Footer.vue'
+import { RouterView } from 'vue-router'
+</script>
+<template>
+  <Header></Header>
+
+  <main class="flex-shrink-0">
+    <div class="container">
+      <RouterView></RouterView>
+    </div>
+  </main>
+
+  <Footer></Footer>
+</template>
