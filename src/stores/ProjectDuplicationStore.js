8a9b97e230faf1a8635be1faa83870b5c9c139bd
William Barnett <will892@LAPTOP-GCQMH64J>
2024-05-24 18:00:05 -0700

    Made creating a request functional

--- src/stores/ProjectDuplicationStore.js
+++ src/stores/ProjectDuplicationStore.js
@@ -1,33 +1,33 @@
-import axios from "axios"
-import { defineStore } from "pinia"
+import axios from 'axios'
+import { defineStore } from 'pinia'
 
 export const useProjectDuplicationStore = defineStore({
-    id: 'project-duplication-request',
-    state: () => ({
-        isLoaded: false,
-        isPublished: false,
-        onetimeMedia: [],
-    }),
+  id: 'project-duplication-request',
+  state: () => ({
+    isLoaded: false,
+    isPublished: false,
+    onetimeMedia: [],
+    userAccess: false,
+  }),
 
-    actions: {
-        async sendRequest(projectId, message) {
+  actions: {
+    async sendRequest(projectId, remarks, transfer) {
+      const url = `${
+        import.meta.env.VITE_API_URL
+      }/projects/${projectId}/duplication/request`
+      await axios.post(url, { remarks, transfer })
+    },
+    async checkForConditions(projectId) {
+      const url = `${
+        import.meta.env.VITE_API_URL
+      }/projects/${projectId}/duplication/request`
+      const response = await axios.get(url)
+      console.log('first')
 
-            const url = `${import.meta.env.VITE_API_URL}/projects/${projectId}/duplication/request`
-            await axios.post(url, { message })
-
-        },
-        async checkForConditions(projectId){
-            const url = `${import.meta.env.VITE_API_URL}/projects/${projectId}/duplication/request`
-            const response = await axios.get(url)
-            console.log('first')
-
-            this.onetimeMedia = response.data.oneTimeMedia
-            this.isPublished = response.data.projectPublished
-            this.isLoaded = true
-
-        }
-
-        
-    }
-    
+      this.onetimeMedia = response.data.oneTimeMedia
+      this.isPublished = response.data.projectPublished
+      this.userAccess = response.data.hasAccess
+      this.isLoaded = true
+    },
+  },
 })
--- src/views/project/duplication/RequestView.vue
+++ src/views/project/duplication/RequestView.vue
@@ -1,5 +1,4 @@
 <script setup>
-// import
 import { computed, ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import router from '@/router'
@@ -8,143 +7,91 @@ import { useProjectUsersStore } from '@/stores/ProjectUsersStore'
 import { useUserStore } from '@/stores/UserStore'
 import LoadingIndicator from '@/components/project/LoadingIndicator.vue'
 
-// compute loading screen
 const route = useRoute()
 const projectId = route.params.id
 const message = ref([null])
-let errorMessage = null
+let transfer = ref(false)
 const ProjectDuplicationStore = useProjectDuplicationStore()
-const ProjectUsersStore = useProjectUsersStore()
-const UserStore = useUserStore()
-const isLoaded = computed(
-    () =>
-        ProjectDuplicationStore.isLoaded &&
-        ProjectUsersStore.isLoaded
-)
+const isLoaded = computed(() => ProjectDuplicationStore.isLoaded)
 
-// onmounted
 onMounted(() => {
-    if(!ProjectDuplicationStore.isLoaded){
-        ProjectDuplicationStore.checkForConditions(projectId)
-    }
-
-    if(!ProjectUsersStore.isLoaded){
-        ProjectUsersStore.fetchUsers(projectId)
-    }
+  if (!ProjectDuplicationStore.isLoaded) {
+    ProjectDuplicationStore.checkForConditions(projectId)
+  }
 })
 
-// functions
-async function makeRequest() {
-    if(message.length == 0) {
-        // reason why cannot be empty
-        errorMessage = 'There were errors in your form: Why the user would like to duplicate the project must be at least 1 characters'
-    }
-    else if(ProjectDuplicationStore.onetimeMedia.length > 0) {
-        // print message explaining what it means
-        errorMessage = "There are media licensed for onetime use in this project. " + 
-                       "This license means that the copyright holder has released the media for use in one MorphoBank project only at present. " +
-                       "To honor the copyright holder's wishes MorphoBank only allows the media in question to exist in one project."
-
-        // check if requestie is not a member of the project and display dif message
-        UserStore.fetchCurrentUser()
-        console.log('Project store; ', ProjectUsersStore.users)
-        console.log('User data: ', UserStore.userForm)
-
-        if(!ProjectUsersStore.users.includes(UserStore.userForm)){            
-            // DO NOT SHOW DROPDOWN IF THIS CASE HAPPENS////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            errorMessage = "As a result, this media can not be copied to the new project during project duplication."
-        }
-        else {
-            // if published this media cannot be duplicated 
-            if(ProjectDuplicationStore.isPublished){
-                errorMessage = "Because this project has already been published, the onetime use media can not be copied to the duplicated project."
-
-            } else {
-
-                // else
-                errorMessage ="During project duplication, the media must be moved to the new project or kept in the original project."
-                
-                // media can only be present in one of the two projects so make them choose
-            }    
-            
-            // list the onetime use media
-            for(const media in ProjectDuplicationStore.onetimeMedia){
-                // different routes depending on the last if statement
-                if(!ProjectDuplicationStore.isPublished) {
-                    // something 
-                } else {
-                    // if not published handle what do with this media
-                }
-
-                
-            }
-
-                
-        }
-            
-            
-    }  
-    else {
-        // push back to overview 
-        ProjectDuplicationStore.sendRequest(projectId, message)
-        router.push({ path: `/myprojects/${projectId}/overview` })
-    } 
-    
+async function makeRequest(remark) {
+  if (remark == null || remark.replace(/\s/g, '').length == 0) {
+    alert(
+      'There were errors in your form: Please include the reason for the request'
+    )
+  } else {
+    ProjectDuplicationStore.sendRequest(projectId, remark, transfer.value)
+    router.push({ path: `/myprojects/${projectId}/overview` })
+  }
 }
-
-
 </script>
 
 <template>
-    <LoadingIndicator :isLoaded="isLoaded">
-        <h1 v-if="errorMessage != null">{{ errorMessage }}</h1>
+  <LoadingIndicator :isLoaded="isLoaded">
+    <header>Project: P{{ projectId }} Test</header>
 
-        <header>
-            Project: P{{ projectId }} Test
-        </header>
+    <RouterLink :to="`/myProjects/${projectId}/overview`">
+      <button class="action-bar">Back to Project Overview</button>
+    </RouterLink>
 
-        <RouterLink :to="`/myProjects/${projectId}/overview`">
-            <button class="action-bar">
-                Back to Project Overview
-            </button>
-        </RouterLink>
+    <p>
+      Please use the following form to submit a request to duplicate P{{
+        projectId
+      }}. After submission, a MorphoBank administrator will contact you with
+      further information.
+    </p>
 
-        <p> 
-            Please use the following form to submit a request to duplicate P{{projectId}}. 
-            After submission, a MorphoBank administrator will contact you with further information.
+    <div v-if="ProjectDuplicationStore.onetimeMedia.length > 0">
+      <header>
+        There are media licensed for onetime use in this project. This license
+        means that the copyright holder has released the media for use in one
+        MorphoBank project only at present. To honor the copyright holder's
+        wishes MorphoBank only allows the media in question to exist in one
+        project.
+      </header>
+      <div v-if="!ProjectDuplicationStore.userAccess">
+        <p>
+          As a result, this media can not be copied to the new project during
+          project duplication.
         </p>
-
-        
-
-        <div v-if="ProjectDuplicationStore.onetimeMedia.length > 0">
-            <header>
-                There are media licensed for onetime use in this project. This license means that the copyright holder has released the media
-            for use in one MorphoBank project only at present. To honor the copyright holder's wishes MorphoBank only allows the media in
-            question to exist in one project.
-            </header>
-            <div v-if="!ProjectUsersStore.users.includes(UserStore.userForm)">
-                <p>As a result, this media can not be copied to the new project during project duplication.</p>
-            </div>
-            <div v-else>
-                <div v-if="ProjectDuplicationStore.isPublished">
-                    <p>Because this project has already been published, the onetime use media can not be copied to the duplicated project.</p>
-                </div>
-                <div v-else>
-                    <p>During project duplication, the media must be moved to the new project or kept in the original project.</p>
-                </div>
-            </div>
+      </div>
+      <div v-else>
+        <div v-if="ProjectDuplicationStore.isPublished">
+          <p>
+            Because this project has already been published, the onetime use
+            media can not be copied to the duplicated project.
+          </p>
         </div>
+        <div v-else>
+          <p>
+            During project duplication, the media must be moved to the new
+            project or kept in the original project.
+          </p>
+          <div class="selection-bar">
+            <label class="item">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                @click="transfer = !transfer"
+              />
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
 
-
-        <form>
-            <label>Enter your remarks here:</label>
-            <input type="text" v-model="message">
-            <button type="button" @click="makeRequest">
-                Submit
-            </button>
-        </form>        
-
-    </LoadingIndicator>
+    <form>
+      <label>Enter your remarks here:</label>
+      <input type="text" v-model="message" />
+      <button type="button" @click="makeRequest(message)">Submit</button>
+    </form>
+  </LoadingIndicator>
 </template>
 
 <style scoped></style>
