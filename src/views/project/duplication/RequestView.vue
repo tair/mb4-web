068b48391e9acd0c75e1ab0c5836dabb4ea71dee
William Barnett <will892@LAPTOP-GCQMH64J>
2024-05-28 16:41:21 -0700

    Added router links to media and removed store

--- src/views/project/duplication/RequestView.vue
+++ src/views/project/duplication/RequestView.vue
@@ -1,68 +1,86 @@
 <script setup>
-import { computed, ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import axios from 'axios'
 import router from '@/router'
-import { useProjectDuplicationStore } from '@/stores/ProjectDuplicationStore'
-import { useProjectUsersStore } from '@/stores/ProjectUsersStore'
-import { useUserStore } from '@/stores/UserStore'
-import LoadingIndicator from '@/components/project/LoadingIndicator.vue'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const projectId = route.params.id
-const message = ref([null])
-let transfer = ref(false)
-const ProjectDuplicationStore = useProjectDuplicationStore()
-const isLoaded = computed(() => ProjectDuplicationStore.isLoaded)
+let onetimeMedia = ref([])
+let isPublished = ref(false)
+let userAccess = ref(false)
 
-onMounted(() => {
-  if (!ProjectDuplicationStore.isLoaded) {
-    ProjectDuplicationStore.checkForConditions(projectId)
-  }
+onMounted(async () => {
+  const url = `${
+    import.meta.env.VITE_API_URL
+  }/projects/${projectId}/duplication/request`
+  const response = await axios.get(url)
+
+  onetimeMedia.value = response.data.oneTimeMedia
+  isPublished.value = response.data.projectPublished
+  userAccess.value = response.data.hasAccess
 })
 
-async function makeRequest(remark) {
-  if (remark == null || remark.replace(/\s/g, '').length == 0) {
+async function makeRequest(event) {
+  const formData = new FormData(event.target)
+  const formObject = Object.fromEntries(formData.entries())
+
+  const remarks = formObject.remarks
+  const onetimeAction = formObject.onetimeAction ? formObject.onetimeAction : 1
+
+  if (formObject.onetimeAction == undefined) {
+    formObject.onetimeAction = 1
+  }
+
+  if (
+    formObject.remarks == undefined ||
+    formObject.remarks.trim().length == 0
+  ) {
     alert(
       'There were errors in your form: Please include the reason for the request'
     )
   } else {
-    ProjectDuplicationStore.sendRequest(projectId, remark, transfer.value)
+    const url = `${
+      import.meta.env.VITE_API_URL
+    }/projects/${projectId}/duplication/request`
+
+    await axios.post(url, { projectId, remarks, onetimeAction })
     router.push({ path: `/myprojects/${projectId}/overview` })
   }
 }
 </script>
 
 <template>
-  <LoadingIndicator :isLoaded="isLoaded">
-    <header>Project: P{{ projectId }} Test</header>
+  <header>Project: P{{ projectId }}</header>
 
-    <RouterLink :to="`/myProjects/${projectId}/overview`">
-      <button class="action-bar">Back to Project Overview</button>
-    </RouterLink>
+  <RouterLink :to="`/myProjects/${projectId}/overview`">
+    <button class="action-bar">Back to Project Overview</button>
+  </RouterLink>
 
-    <p>
-      Please use the following form to submit a request to duplicate P{{
-        projectId
-      }}. After submission, a MorphoBank administrator will contact you with
-      further information.
-    </p>
+  <p>
+    Please use the following form to submit a request to duplicate P{{
+      projectId
+    }}. After submission, a MorphoBank administrator will contact you with
+    further information.
+  </p>
 
-    <div v-if="ProjectDuplicationStore.onetimeMedia.length > 0">
-      <header>
-        There are media licensed for onetime use in this project. This license
-        means that the copyright holder has released the media for use in one
-        MorphoBank project only at present. To honor the copyright holder's
-        wishes MorphoBank only allows the media in question to exist in one
-        project.
-      </header>
-      <div v-if="!ProjectDuplicationStore.userAccess">
+  <form @submit.prevent="makeRequest">
+    <div v-if="onetimeMedia.length > 0">
+      <p>
+        There are {{ onetimeMedia.length }} media licensed for onetime use in
+        this project. This license means that the copyright holder has released
+        the media for use in one MorphoBank project only at present. To honor
+        the copyright holder's wishes MorphoBank only allows the media in
+        question to exist in one project.
+      </p>
+      <div v-if="!userAccess">
         <p>
           As a result, this media can not be copied to the new project during
           project duplication.
         </p>
       </div>
       <div v-else>
-        <div v-if="ProjectDuplicationStore.isPublished">
+        <div v-if="isPublished.value">
           <p>
             Because this project has already been published, the onetime use
             media can not be copied to the duplicated project.
@@ -73,25 +91,48 @@ async function makeRequest(remark) {
             During project duplication, the media must be moved to the new
             project or kept in the original project.
           </p>
-          <div class="selection-bar">
+          <p>The following media have been licensed for onetime use:</p>
+          <li v-for="media in onetimeMedia" class="list-group-item">
+            <RouterLink
+              :to="`/myprojects/${projectId}/media/${media.media_id}`"
+            >
+              <button type="button">Onetime Media File</button>
+            </RouterLink>
+          </li>
+          <p>How would you like to handle onetime use media?</p>
+          <div class="list-group-item-name">
             <label class="item">
               <input
-                type="checkbox"
+                type="radio"
                 class="form-check-input"
-                @click="transfer = !transfer"
+                name="onetimeAction"
+                value="100"
               />
+              Move onetime use media to new project
+            </label>
+          </div>
+          <div class="list-group-item-name">
+            <label class="item">
+              <input
+                type="radio"
+                class="form-check-input"
+                name="onetimeAction"
+                value="1"
+                :checked="true"
+              />
+              Keep onetime use media in existing project
             </label>
           </div>
         </div>
       </div>
     </div>
 
-    <form>
-      <label>Enter your remarks here:</label>
-      <input type="text" v-model="message" />
-      <button type="button" @click="makeRequest(message)">Submit</button>
-    </form>
-  </LoadingIndicator>
+    <label>Enter your remarks here:</label>
+    <input type="text" name="remarks" />
+    <button type="submit">Submit</button>
+  </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import '@/views/project/styles.css';
+</style>
