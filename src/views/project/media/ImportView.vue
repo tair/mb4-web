aaa2709319cc09e91f4ff3a305edf194b5ce292e
Kenzley Alphonse <kenzley@users.noreply.github.com>
2024-06-02 10:51:26 -0400

    Implement import media flow

--- src/views/project/media/ImportView.vue
+++ src/views/project/media/ImportView.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
+import router from '@/router'
 import axios from 'axios'
 import { computed, onMounted, reactive, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useTaxaStore } from '@/stores/TaxaStore'
-import Tooltip from '@/components/main/Tooltip.vue'
-import LoadingIndicator from '@/components/project/LoadingIndicator.vue'
-import TaxonomicName from '@/components/project/TaxonomicName.vue'
 import { TaxaColumns, getTaxonName } from '@/utils/taxa'
 import { Filter } from '@/types/most'
 import { toISODate } from '@/utils/date'
 
+import ImportMediaComponent from '@/components/project/ImportMediaComponent.vue'
+import LoadingIndicator from '@/components/project/LoadingIndicator.vue'
+import TaxonomicName from '@/components/project/TaxonomicName.vue'
+import Tooltip from '@/components/main/Tooltip.vue'
+
 interface ImportInfo {
   no_results_on: number | null
   set_on: number | null
+}
+
+interface ImportResult {
+  taxon_id: number
+  link: string
+  media: ImportMedia[]
 }
 
 interface ImportMedia {
@@ -21,6 +30,9 @@ interface ImportMedia {
   copyright_info: string
   copyright_permission: number
   copyright_license: number
+  imported?: boolean
+  should_import?: boolean
+  should_add_as_exemplar?: boolean
 }
 
 const route = useRoute()
@@ -52,8 +64,12 @@ const isLoaded = computed(() => taxaStore.isLoaded)
 const hasNoResult = computed(() =>
   Array.from(importResults.value.values()).some((v) => v.no_results_on > 0)
 )
-const importResults = ref<Map<number, ImportInfo>>(new Map())
-const mediaResults = ref<Map<number, ImportMedia[]>>(new Map())
+const importResults = ref<Map<number, ImportInfo>>(
+  new Map<number, ImportInfo>()
+)
+const mediaResults = ref<Map<number, ImportResult>>(
+  new Map<number, ImportResult>()
+)
 
 function moveToStep(step: string) {
   const steps = document.getElementsByClassName('btn-circle')
@@ -121,7 +137,17 @@ function unselectAll() {
   return false
 }
 
-async function importSelectedTaxa() {
+function selectAllForImport(shouldImport = true, shouldAddAsExemplar = true) {
+  const resultsValues = Array.from(mediaResults.value.values())
+  for (const results of resultsValues) {
+    for (const result of results.media) {
+      result.should_import = shouldImport
+      result.should_add_as_exemplar = shouldAddAsExemplar
+    }
+  }
+}
+
+async function fecthMediaForSelectedTaxa() {
   const selectedTaxaIds = taxaStore.taxa
     .filter((taxon) => taxon.selected)
     .map((taxon) => taxon.taxon_id)
@@ -137,7 +163,7 @@ async function importSelectedTaxa() {
   }
 
   const response = await axios.post(
-    `${import.meta.env.VITE_API_URL}/projects/${projectId}/${importType}/fetch`,
+    `${import.meta.env.VITE_API_URL}/projects/${projectId}/${importType}/media`,
     { taxon_ids: selectedTaxaIds }
   )
 
@@ -154,6 +180,78 @@ async function importSelectedTaxa() {
 
   moveToStep('step-2')
   return false
+}
+
+async function fetchMoreMedia(taxonId: number) {
+  const results = mediaResults.value.get(taxonId)
+  const response = await axios.post(
+    `${import.meta.env.VITE_API_URL}/projects/${projectId}/${importType}/media`,
+    { taxon_ids: [taxonId], size: results.media.length + 9 }
+  )
+
+  if (response.status != 200) {
+    alert('Failed to fetch more media')
+    return
+  }
+
+  for (const result of response.data.results) {
+    mediaResults.value.set(taxonId, result)
+  }
+}
+
+async function importMediaForSelectedTaxa() {
+  // Iterate through the media results and only import values that were selected
+  // by the user.
+  const selected: ImportResult[] = []
+  const resultsValues = Array.from(mediaResults.value.values())
+  for (const resultValue of resultsValues) {
+    const taxonId = resultValue.taxon_id
+    const link = resultValue.link
+    const media: ImportMedia[] = []
+    for (const resultMedia of resultValue.media) {
+      if (resultMedia.should_import) {
+        media.push(resultMedia)
+      }
+    }
+    if (media.length > 0) {
+      selected.push({
+        taxon_id: taxonId,
+        link,
+        media,
+      })
+    }
+  }
+
+  if (selected.length == 0) {
+    alert('Please select media to import')
+  }
+
+  const response = await axios.post(
+    `${
+      import.meta.env.VITE_API_URL
+    }/projects/${projectId}/${importType}/import`,
+    { imports: selected }
+  )
+
+  if (response.status != 200) {
+    alert('Failed to import the media')
+    return
+  }
+
+  if (!response.data.success) {
+    alert('Failed to import media: ' + response.data.message ?? 'Unknown issue')
+    return
+  }
+
+  // Clear the import list and media results since media have been imported
+  // thus these are invalidated.
+  mediaResults.value.clear()
+  importResults.value.clear()
+
+  alert(
+    'You have successfully imported the media into Morphobank but you curate them before they can be released into your import.'
+  )
+  router.push({ path: `/myprojects/${projectId}/media/curate` })
 }
 
 onMounted(async () => {
@@ -174,7 +272,10 @@ onMounted(async () => {
   <LoadingIndicator :isLoaded="isLoaded">
     <div class="nav-link d-flex align-items-center fw-bold small m-0 p-0 mb-3">
       <i class="fa-solid fa-chevron-left"></i>
-      <RouterLink :to="`/myprojects/${projectId}/media/`" class="nav-link m-0 p-0 pl-1">
+      <RouterLink
+        :to="`/myprojects/${projectId}/media/`"
+        class="nav-link m-0 p-0 pl-1"
+      >
         Back to Media
       </RouterLink>
     </div>
@@ -189,16 +290,15 @@ onMounted(async () => {
           <p>Select</p>
         </div>
         <div class="import-step">
-          <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">
+          <a
+            href="#step-2"
+            type="button"
+            class="btn btn-default btn-circle"
+            disabled="disabled"
+          >
             2
           </a>
           <p>Import</p>
-        </div>
-        <div class="import-step">
-          <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">
-            3
-          </a>
-          <p>Curate</p>
         </div>
       </div>
       <div>
@@ -218,7 +318,8 @@ onMounted(async () => {
                 Select records with no results
               </button>
               <Tooltip
-                :content="`Use this to search ${importText} again since their database is constantly being updated (Only first 100 will be selected)`">
+                :content="`Use this to search ${importText} again since their database is constantly being updated (Only first 100 will be selected)`"
+              >
               </Tooltip>
             </div>
             <div>
@@ -229,8 +330,9 @@ onMounted(async () => {
               for on {{ importText }}. You will be presented image results to
               selectively import for use as
               <b>taxon exemplars</b>
-              <Tooltip content="Taxon exemplars appear in matrices to illustrate a taxon" />. Unvouchered specimens will
-              be created for these new media
+              <Tooltip
+                content="Taxon exemplars appear in matrices to illustrate a taxon"
+              />. Unvouchered specimens will be created for these new media
               files if they are not already available for the taxon.
               <br />
               <i>
@@ -239,60 +341,103 @@ onMounted(async () => {
               </i>
             </div>
             <div class="grid">
-              <div v-for="taxon in filteredTaxa" :key="taxon.taxon_id" class="grid-item">
-                <input class="form-check-input" :id="'ci' + taxon.taxon_id" type="checkbox" v-model="taxon.selected" />
+              <div
+                v-for="taxon in filteredTaxa"
+                :key="taxon.taxon_id"
+                class="grid-item"
+              >
+                <input
+                  class="form-check-input"
+                  :id="'ci' + taxon.taxon_id"
+                  type="checkbox"
+                  v-model="taxon.selected"
+                />
                 <label class="grid-item-name" :for="'ci' + taxon.taxon_id">
-                  <TaxonomicName :class="{
+                  <TaxonomicName
+                    :class="{
                       greyed:
                         importResults.get(taxon.taxon_id)?.no_results_on ||
                         importResults.get(taxon.taxon_id)?.set_on,
-                    }" :showExtinctMarker="true" :taxon="taxon" />
-                  <span v-if="importResults.get(taxon.taxon_id)?.set_on" class="already-imported">
+                    }"
+                    :showExtinctMarker="true"
+                    :taxon="taxon"
+                  />
+                  <span
+                    v-if="importResults.get(taxon.taxon_id)?.set_on"
+                    class="already-imported"
+                  >
                     already imported
                   </span>
-                  <span v-else-if="importResults.get(taxon.taxon_id)?.no_results_on" class="no-results">
+                  <span
+                    v-else-if="importResults.get(taxon.taxon_id)?.no_results_on"
+                    class="no-results"
+                  >
                     no results ({{
-                    toISODate(
-                    importResults.get(taxon.taxon_id)?.no_results_on
-                    )
+                      toISODate(
+                        importResults.get(taxon.taxon_id)?.no_results_on
+                      )
                     }})
                   </span>
                 </label>
               </div>
             </div>
             <div class="btn-step-group">
-              <button type="button" class="btn btn-primary btn-step-prev" @click="$router.go(-1)">
+              <button
+                type="button"
+                class="btn btn-primary btn-step-prev"
+                @click="$router.go(-1)"
+              >
                 Cancel
               </button>
-              <button type="button" class="btn btn-primary btn-step-next" @click="importSelectedTaxa">
+              <button
+                type="button"
+                class="btn btn-primary btn-step-next"
+                @click="fecthMediaForSelectedTaxa"
+              >
                 Next
               </button>
             </div>
           </div>
           <div class="row setup-content" id="step-2">
-            <div v-if="mediaResults.size">
-              <div v-for="[taxonId, result] in mediaResults" :key="taxonId" class="grid-item">
-                {{  result }}
+            <div v-if="mediaResults.size > 0">
+              <div>
+                <button type="button" @click="selectAllForImport(true, false)">
+                  Select all for import
+                </button>
+                <button type="button" @click="selectAllForImport(true, true)">
+                  Select all for import/exemplar
+                </button>
+                <button type="button" @click="selectAllForImport(false, false)">
+                  Deselect all
+                </button>
+              </div>
+              <div
+                v-for="[taxonId, results] in mediaResults"
+                :key="taxonId"
+                class="grid-item"
+              >
+                <ImportMediaComponent
+                  :taxon="taxaStore.getTaxonById(taxonId)"
+                  :importText="importText"
+                  :results="results"
+                  :fetchMoreMedia="() => fetchMoreMedia(taxonId)"
+                />
               </div>
             </div>
-            <div v-else>
-              No results from {{ importText }}
-            </div>
+            <div v-else>No results from {{ importText }}</div>
             <div class="btn-step-group">
-              <button class="btn btn-primary btn-step-prev" type="button" @click="moveToStep('step-1')">
+              <button
+                class="btn btn-primary btn-step-prev"
+                type="button"
+                @click="moveToStep('step-1')"
+              >
                 Prev
               </button>
-              <button type="button" class="btn btn-primary btn-step-next" @click="moveToStep('step-3')">
-                Next
-              </button>
-            </div>
-          </div>
-          <div class="row setup-content" id="step-3">
-            <div class="btn-step-group">
-              <button class="btn btn-primary btn-step-prev" type="button" @click="moveToStep('step-2')">
-                Prev
-              </button>
-              <button class="btn btn-primary btn-step-next" type="submit">
+              <button
+                type="button"
+                class="btn btn-primary btn-step-next"
+                @click="importMediaForSelectedTaxa"
+              >
                 Import
               </button>
             </div>
@@ -332,5 +477,13 @@ onMounted(async () => {
 
 .greyed {
   color: #828282;
+}
+
+.grid-item:nth-of-type(even) {
+  background-color: #fff;
+}
+
+.grid-item:nth-of-type(odd) {
+  background-color: #f7f7f7;
 }
 </style>
