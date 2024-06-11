879250d94b04dc6d47fe61f000e98fc3294653c9
William Barnett <will892@LAPTOP-GCQMH64J>
2024-06-10 17:14:37 -0700

    Working functionality of publish partitions

--- src/views/project/publishPartition/PublishPartitionView.vue
+++ src/views/project/publishPartition/PublishPartitionView.vue
@@ -1,36 +1,4 @@
 <script setup>
-/*
-    Template layout
-    =================================================================
-    1. form
-        - print a summary line
-        - inform user
-            -You can publish a subset of your project data by defining a partition in the matrix editor and then exporting that partition to a new MorphoBank project using the controls below.
-            -*NOTE* The new partition will not be published live immediately upon creation. It will be listed among your unpublished projects and you will then need to follow the regular steps for publication.
-            -The partition publishing process copies all of the taxa and characters in your partition along with any partition-associated matrix data, media, specimens, views, labels, character notes and cell notes. All bibliographic references and documents from the project, regardless of partition, will be copied as well, as will any media related to project bibliographic references. The result will be a new project separate and independent from this project containing only the partition-defined subset described above. Subsequent changes made in either this project or the published partition will not be reflected in the other.
-            -Please use this tool only when you intend to publish something as server space is finite. 	
-        - ask user for a partition to publish
-        - make a submit button that calls a function to publish said partition
-    2. header
-        - print partition publishing
-    3. partition result
-        - check if success of submitting request
-            - IF YES : your request to publish the {name} partition in P('projectId') has been submitted. You will receive an email notification at ('email') when duplication of the partition is completed.
-        	- if no: Error copying partition: (error) // maybe keep
-
-
-    
-    Setup layout
-    =================================================================
-    1. form 
-        - get information for each partition associated with this project 
-    2. header
-        - make it do the print
-    3. partition result 
-        - get partition info to print
-        - get whether it was successful or not 
-        - try to tell user what went wrong with the request
-*/
 import axios from 'axios'
 import router from '@/router'
 import { ref, onMounted } from 'vue'
--- src/views/project/publishPartition/SummaryView.vue
+++ src/views/project/publishPartition/SummaryView.vue
@@ -1,122 +1,162 @@
 <script setup>
-/*
-    4. Summary (dear lord...)
-        - print partition name
-        - display partition contents
-            - just all the nums of what was retrieved
-        - display to the user that if they wish to edit any of this then they must do so in the matrix editor
-        - if onetime media is present
-            - inform the user: This partition contains {vn_num_one_time_use_media} media released for onetime use. Due to this license, these media may not be usable in a derivative project. You must either change the media reuse license, or choose to keep the media in the current project, the new parition based project or both (bypassing one-time use).  
-                - Click here to see a list of all onetime use media.
-                - display three options 
-                    - Keep one-time use media in existing project
-                    - Move one-time use media to new partition based project
-                    - Use one-time use media in both existing project and new partition based project
-        - print the help texts
-            - When you publish this partition, a new project will be created with the characters and taxa listed above. Any matrices, media, copyright documents and bibliographic
-			  citations associated with the characters and taxa will be copied into the new project. The new project will be left in an unpublished state to allow you time to review
-			  prior to publication.
-            - Note that the new project you create here will be separate from P<?php print $t_project->getPrimaryKey(); ?> (<?php print $t_project->get('name'); ?>) and create
-			  only the subset of information described above. Changes made to the newly created project will not affect P<?php print $t_project->getPrimaryKey(); ?> or vice versa.
-            - Note that publishing a partition may consume significant server resources, particularly storage. Please be considerate and only publish
-			  when you need to.
-        - print if partition is empty: You cannot publish this partition because it is empty</strong>. Please add characters and taxa and try again.
-        - else show a button that allows publishment
-
-    4. Summary (dear lord...)
-        - get taxa and characters from the partition 
-        - get num of matricies and get media specimens and views 
-        - get num of media, onetime media, specimens, and views
-            - make it possible to get links to each onetimeMedia when requested
-            - make it possible to retrieve the decision with what has to be done for the onetimeMedia
-        - get num of documents, labels, and bib references
-        - create new taxa?
-        - check if the partition is empty 
-*/
 import axios from 'axios'
 import router from '@/router'
-import { ref, onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import LoadingIndicator from '@/components/project/LoadingIndicator.vue'
 
 const route = useRoute()
 const projectId = route.params.id
 const partitionId = route.params.partitionId
+const isLoaded = ref(false)
 
 let partition = null
-let taxa = 0
-let characters = 0
-let onetimeMedia = []
-let media = null
-let labels = 0
-let documents = 0
-let bibliographicReferences = 0
-let views = 0
-let specimens = 0
+let taxa = ref(0)
+let characters = ref(0)
+let onetimeMedia = ref([])
+let media = ref(null)
+let labels = ref(0)
+let documents = ref(0)
+let bibliographicReferences = ref(0)
+let views = ref(0)
+let specimens = ref(0)
 
 onMounted(async () => {
   const url = `${
     import.meta.env.VITE_API_URL
-  }/myprojects/${projectId}/publish/partition/${partitionId}`
+  }/projects/${projectId}/publish/partition/${partitionId}`
 
   const response = await axios.get(url)
 
   partition = response.data.partition
-  taxa = response.data.taxaLength
-  characters = response.data.characterLength
-  onetimeMedia = response.data.onetimeMedia
-  media = response.data.mediaLength
-  labels = response.data.labels
-  documents = response.data.documents
-  bibliographicReferences = response.data.bibliographicReferences
-  views = response.data.views
-  specimens = response.data.specimens
+  taxa.value = response.data.taxaLength
+  characters.value = response.data.characterLength
+  onetimeMedia.value = response.data.onetimeMedia
+  media.value = response.data.mediaLength
+  labels.value = response.data.labels
+  documents.value = response.data.documents
+  bibliographicReferences.value = response.data.bibliographicReferences
+  views.value = response.data.views
+  specimens.value = response.data.specimens
+
+  isLoaded.value = true
 })
 
 async function publishPartition(event) {
+  const formData = new FormData(event.target)
+  const formObject = Object.fromEntries(formData.entries())
+
+  const onetimeAction = formObject.onetimeAction || 1
+
   const url = `${
     import.meta.env.VITE_API_URL
-  }/projects/${projectId}/publish/partition`
-  const response = await axios.post(url, { partitionId })
+  }/projects/${projectId}/publish/partition/${partitionId}`
+  const response = await axios.post(url, { onetimeAction })
 
   if (response.status == 200) {
-    // whatever logic goes here
+    alert('Publishing Partition')
+    router.push({ path: `/myprojects/${projectId}/overview` })
   } else {
-    // display to the user what exactly went wrong
+    alert('Could not publish Partition')
   }
 }
 </script>
 <template>
-  <h1>Partition: {{ partition.name }}</h1>
-  <p>
-    This partition contains {{ characters }} characters, {{ taxa }} taxa,
-    {{ media }} media of which {{ onetimeMedia.length }} are released for
-    onetime use, {{ labels }} media labels, {{ specimens }} specimens,
-    {{ views }} views, {{ documents }} documents that are linked to media
-    explaining copyright and {{ bibliographicReferences }} bibliographic
-    references To edit these, go to the Matrix Editor and make changes
-  </p>
-  <p>
-    When you publish this partition, a new project will be created with the
-    characters and taxa listed above. Any matrices, media, copyright documents
-    and bibliographic citations associated with the characters and taxa will be
-    copied into the new project. The new project will be left in an unpublished
-    state to allow you time to review prior to publication.
-  </p>
-  <p>
-    Note that the new project you create here will be separate from P4091 (Test)
-    and create only the subset of information described above. Changes made to
-    the newly created project will not affect P4091 or vice versa.
-  </p>
-  <p>
-    Note that publishing a partition may consume significant server resources,
-    particularly storage. Please be considerate and only publish when you need
-    to.
-  </p>
+  <LoadingIndicator :isLoaded="isLoaded">
+    <RouterLink :to="`/myprojects/${projectId}/publish/partition`">
+      <button type="button">Go Back</button>
+    </RouterLink>
 
-  <div v-if="taxa && characters">
+    <h1>Partition: {{ partition.name }}</h1>
     <p>
-      You cannot publish this partition because it is empty. Please add
-      characters and taxa and try again.
+      This partition contains {{ characters }} characters, {{ taxa }} taxa,
+      {{ media }} media of which {{ onetimeMedia.length }} are released for
+      onetime use, {{ labels }} media labels, {{ specimens }} specimens,
+      {{ views }} views, {{ documents }} documents that are linked to media
+      explaining copyright and {{ bibliographicReferences }} bibliographic
+      references To edit these, go to the Matrix Editor and make changes
     </p>
-  </div>
+    <p>
+      When you publish this partition, a new project will be created with the
+      characters and taxa listed above. Any matrices, media, copyright documents
+      and bibliographic citations associated with the characters and taxa will
+      be copied into the new project. The new project will be left in an
+      unpublished state to allow you time to review prior to publication.
+    </p>
+    <p>
+      Note that the new project you create here will be separate from P4091
+      (Test) and create only the subset of information described above. Changes
+      made to the newly created project will not affect P4091 or vice versa.
+    </p>
+    <p>
+      Note that publishing a partition may consume significant server resources,
+      particularly storage. Please be considerate and only publish when you need
+      to.
+    </p>
+
+    <div v-if="!taxa || !characters">
+      <p>
+        You cannot publish this partition because it is empty. Please add
+        characters and taxa and try again.
+      </p>
+    </div>
+
+    <div v-else>
+      <form @submit.prevent="publishPartition">
+        <div v-if="onetimeMedia.length">
+          <p>
+            This partition contains {onetimeMedia.length} media released for
+            onetime use. Due to this license, these media may not be usable in a
+            derivative project. You must either change the media reuse license,
+            or choose to keep the media in the current project, the new parition
+            based project or both (bypassing one-time use).
+          </p>
+          <li v-for="media in onetimeMedia" class="list-group-item">
+            <RouterLink
+              :to="`/myprojects/${projectId}/media/${media.media_id}`"
+            >
+              <button type="button">Onetime Media File</button>
+            </RouterLink>
+          </li>
+          <p>How would you like to handle onetime use media?</p>
+          <div class="list-group-item-name">
+            <label class="item">
+              <input
+                type="radio"
+                class="form-check-input"
+                name="onetimeAction"
+                value="1"
+                :checked="true"
+              />
+              Keep onetime use media in existing project
+            </label>
+          </div>
+          <div class="list-group-item-name">
+            <label class="item">
+              <input
+                type="radio"
+                class="form-check-input"
+                name="onetimeAction"
+                value="100"
+              />
+              Move onetime use media to new project
+            </label>
+          </div>
+          <div class="list-group-item-name">
+            <label class="item">
+              <input
+                type="radio"
+                class="form-check-input"
+                name="onetimeAction"
+                value="1"
+              />
+              Use one-time use media in both existing project and new partition
+              based project
+            </label>
+          </div>
+        </div>
+        <button type="submit">Publish</button>
+      </form>
+    </div>
+  </LoadingIndicator>
 </template>
