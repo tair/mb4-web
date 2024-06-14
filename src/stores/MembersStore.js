048a8516de724479b5e4adcfe6d10371d2fe6cf4
joearias1121 <joearias1121@gmail.com>
2024-06-13 22:42:37 -0400

    setting up the store

--- src/stores/MembersStore.js
+++ src/stores/MembersStore.js
@@ -17,6 +17,27 @@ export const useMembersStore = defineStore({
       this.members = response.data.members
       this.isLoaded = true
     },
+    async deleteMembers(projectId, linkIds) {
+      const url = `${
+        import.meta.env.VITE_API_URL
+      }/projects/${projectId}/members/delete`
+      const response = await axios.post(url, {
+        link_ids: linkIds,
+      })
+      if (response.status == 200) {
+        this.removeMemberById(linkIds)
+        return true
+      }
+      return false
+    },
+    removeMemberById(linkIds) {
+      for (let x = 0; x < this.members.length; ++x) {
+        if (linkIds.includes(this.members[x].link_id)) {
+          this.members.splice(x, 1)
+          break
+        }
+      }
+    },
     invalidate() {
       this.members = []
       this.isLoaded = false
