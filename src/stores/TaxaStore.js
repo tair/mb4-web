cd4869bd56046a578f2d0aa05e9be09dfc865848
Kenzley Alphonse <kenzley@users.noreply.github.com>
2024-06-15 18:53:31 -0400

    Implement batch editting for taxa

--- src/stores/TaxaStore.js
+++ src/stores/TaxaStore.js
@@ -73,6 +73,22 @@ export const useTaxaStore = defineStore({
       }
       return false
     },
+    async editIds(projectId, taxaIds, json) {
+      const url = `${
+        import.meta.env.VITE_API_URL
+      }/projects/${projectId}/taxa/edit`
+      const response = await axios.post(url, {
+        taxa_ids: taxaIds,
+        taxa: json,
+      })
+      if (response.status == 200) {
+        const taxa = response.data.taxa
+        this.removeByTaxonIds(taxaIds)
+        this.taxa.push(...taxa)
+        return true
+      }
+      return false
+    },
     async deleteIds(projectId, taxonIds, remappedTaxonIds) {
       const url = `${
         import.meta.env.VITE_API_URL
