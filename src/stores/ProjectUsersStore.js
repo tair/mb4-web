88a53aa42d2adfca956acfb6b447fbb22e6b6860
joearias1121 <joearias1121@gmail.com>
2024-06-23 20:06:20 -0400

    consolidated member store into project user store

--- src/stores/ProjectUsersStore.js
+++ src/stores/ProjectUsersStore.js
@@ -11,12 +11,19 @@ export const useProjectUsersStore = defineStore({
   state: () => ({
     isLoaded: false,
     users: [],
+    members:[],
   }),
   actions: {
     async fetchUsers(projectId) {
       const url = `${import.meta.env.VITE_API_URL}/projects/${projectId}/users`
       const response = await axios.get(url)
       this.users = response.data.users
+
+      const ur = `${
+        import.meta.env.VITE_API_URL
+      }/projects/${projectId}/users/members`
+      const res = await axios.get(ur)
+      this.members = res.data.members
       this.isLoaded = true
     },
     getUserById(userId) {
@@ -36,9 +43,31 @@ export const useProjectUsersStore = defineStore({
       }
       return users
     },
+    async deleteMember(projectId, linkId) {
+      const url = `${
+        import.meta.env.VITE_API_URL
+      }/projects/${projectId}/users/delete`
+      const response = await axios.post(url, {
+        link_id: linkId,
+      })
+      if (response.status == 200) {
+        this.removeMemberById(linkId)
+        return true
+      }
+      return false
+    },
+    removeMemberById(linkId) {
+      for (let x = 0; x < this.members.length; ++x) {
+        if (linkId == this.members[x].link_id) {
+          this.members.splice(x, 1)
+          break
+        }
+      }
+    },
     invalidate() {
       this.isLoaded = false
       this.users = []
+      this.members = []
     },
   },
 })
