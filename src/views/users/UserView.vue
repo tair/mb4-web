cb2b1a40152e4028f25125c52317160cb789b8a0
MsXN31 <xingguo.chen@arabidopsis.org>
2024-07-16 19:54:02 -0700

    Dev release 20240717 (#101)
    * MB4-65: Implement the list media views page of a published project.
    
    * MB4-176: Add reset password feature.

--- src/views/users/UserView.vue
+++ src/views/users/UserView.vue
@@ -5,3 +5,21 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView></RouterView>
 </template>
+
+<style>
+.input-container {
+  margin-bottom: 20px;
+  padding: 5px;
+}
+.input-container label {
+  margin-bottom: 10px;
+}
+
+.input-field {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 1px;
+  width: 100%;
+}
+</style>
