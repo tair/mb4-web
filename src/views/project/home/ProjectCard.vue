bfe5197fff625d54bc85d00d4c308c5d79c0f327
MsXN31 <xingguo.chen@arabidopsis.org>
2024-10-10 16:06:26 -0700

    Dev release 20241010 (#128)
    * MB4-225: Fix document download file name.
    
    * Update My Projects page layout.

--- src/views/project/home/ProjectCard.vue
+++ src/views/project/home/ProjectCard.vue
@@ -28,51 +28,70 @@ function getOverviewUrl() {
 <template>
   <div class="card mb-3">
     <div class="row g-0">
-      <div class="col-md-4">
-        <img
-          v-if="project.media"
-          :src="project.media.url"
-          :width="project.media.width"
-          :height="project.media.height"
-          class="img-fluid rounded-start"
-        />
-        <div v-else>Add your exemplar media</div>
+      <div class="col-md-3">
+        <div class="card-image">
+          <img
+            v-if="project.media"
+            :src="project.media.url"
+            :width="project.media.width"
+            :height="project.media.height"
+            class="img-fluid rounded-start"
+          />
+          <div v-else>Add your exemplar media</div>
+        </div>
       </div>
-      <div class="col-md-8">
+      <div class="col-md-9">
         <div class="card-body">
-          <h5 class="card-title">{{ project.name }}</h5>
+          <h5 class="card-title text-black-50">
+            {{ `Project ${project.project_id}: ${project.name}` }}
+          </h5>
           <p class="card-text">
             {{ getProjectCitation(project) }}
           </p>
-          <div class="card-text">
-            <b>{{ project.members?.length ?? 0 }} members:</b>
+          <div class="card-text mb-2">
+            <b class="me-2">{{ project.members?.length ?? 0 }} members:</b>
             <small class="text-muted" v-if="project.members">
               {{ project.members.map((m) => m.name).join(', ') }}
             </small>
           </div>
           <div class="card-text">
-            <b>Creation Date:</b>
+            <b class="me-2">Creation Date:</b>
             <small class="text-muted">
               {{ toDMYDate(project.created_on) }}
             </small>
           </div>
           <div class="card-text">
-            <b>Last Modified Date:</b>
+            <b class="me-2">Last Modified Date:</b>
             <small class="text-muted">
               {{ toDMYDate(project.last_accessed_on) }}
             </small>
           </div>
           <div class="card-text">
-            <b>Your last Access:</b>
+            <b class="me-2">Your last Access:</b>
             <small class="text-muted">
               {{ toDMYDate(project.user_last_accessed_on) }}
             </small>
           </div>
-          <RouterLink :to="getOverviewUrl()">
-            <button type="button" class="btn btn-primary">View</button>
-          </RouterLink>
+          <div class="mt-3">
+            <RouterLink :to="getOverviewUrl()">
+              <button type="button" class="btn btn-primary">
+                View Project >>
+              </button>
+            </RouterLink>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.card-image {
+  height: 200px;
+  margin: 20px;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: center; /* Centers horizontally */
+  align-items: center; /* Centers vertically */
+}
+</style>
