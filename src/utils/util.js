400c2154c2a4f44fff5f70c769b80fd862690c1d
MsXN31 <xingguo.chen@arabidopsis.org>
2024-11-21 07:50:52 -0800

    Update some text and fix project cover display. (#134)

--- src/utils/util.js
+++ src/utils/util.js
@@ -20,7 +20,7 @@ function getPasswordPattern() {
 }
 
 function getPasswordRule() {
-  return 'Password must be 8 or more characters long, have at least 1 number, 1 uppercase letter, 1 lowercase letter, and 1 special character.'
+  return 'Password must be 8 or more characters long, have at least 1 number, 1 uppercase letter, 1 lowercase letter, and 1 special character (@$!%*?&).'
 }
 
 function searchInObject(obj, searchStr, includeFields = []) {
