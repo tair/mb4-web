d6c023121c5390a50e41d883d065d4294d30d6fd
Kartik <kartik.khosa@phxbio.org>
2025-07-23 16:54:09 -0500

    revert accidental change on that one file

--- src/views/project/overview/ProjectSummary.vue
+++ src/views/project/overview/ProjectSummary.vue
@@ -99,173 +99,163 @@ function popDownloadAlert() {
 </script>
 
 <template>
-  <!-- Loading state while data is being fetched -->
-  <div v-if="!overview || !overview.stats" class="d-flex justify-content-center align-items-center" style="min-height: 200px;">
-    <div class="spinner-border" role="status">
-      <span class="visually-hidden">Loading...</span>
+  <div class="row">
+    <div class="col">
+      <div class="thumbnil-block p-2">
+        <!-- Use exemplar media from overview data -->
+        <div v-if="exemplarMedia" class="d-flex flex-column">
+          <div class="d-flex justify-content-center">
+            <img
+              :src="buildMediaUrl(projectId, exemplarMedia.media_id, 'large')"
+              :style="{
+                width: getWidth(exemplarMedia),
+                height: getHeight(exemplarMedia),
+                maxWidth: '100%',
+                maxHeight: '300px',
+                objectFit: 'contain',
+                backgroundSize: '20px',
+                backgroundRepeat: 'no-repeat',
+                backgroundImage: 'url(' + '/images/loader.png' + ')',
+                backgroundPosition: '10px 10px',
+              }"
+              class="col-md-4 mt-1 ms-0 rounded float-sm-start"
+            />
+          </div>
+          <div class="text-block">
+            <div
+              v-if="exemplarMedia.specimen_name"
+              v-html="'Specimen: ' + exemplarMedia.specimen_name"
+            ></div>
+            <div v-if="exemplarMedia.view_name">
+              View: {{ exemplarMedia.view_name }}
+            </div>
+          </div>
+        </div>
+        <div v-else class="thumbnil-block imgPlaceholderOverview p-2">
+          Project Administrator should add exemplar media
+        </div>
+        <div class="mt-3">
+          <router-link to="media"> More Images </router-link>
+        </div>
+      </div>
+
+      <h4>Abstract</h4>
+      <p v-html="overview.description"></p>
+
+      <div class="mt-5" v-if="overview.article_doi">
+        <span class="fw-bold">Article DOI:</span>
+        {{ overview.article_doi }}
+        <a
+          v-if="overview.journal_url"
+          :href="overview.journal_url"
+          target="_blank"
+        >
+          <button class="btn btn-primary btn-ml">Read the article</button>
+        </a>
+      </div>
+
+      <div v-if="overview.project_doi">
+        <span class="fw-bold">Project DOI:</span>
+        {{ overview.project_doi }},
+        <a :href="`http://dx.doi.org/${overview.project_doi}`">
+          http://dx.doi.org/{{ overview.project_doi }}
+        </a>
+      </div>
     </div>
   </div>
 
-  <!-- Main content once data is loaded -->
-  <div v-else>
-    <div class="row">
-      <div class="col">
-        <div class="thumbnil-block p-2">
-          <!-- Use exemplar media from overview data -->
-          <div v-if="exemplarMedia" class="d-flex flex-column">
-            <div class="d-flex justify-content-center">
-              <img
-                :src="buildMediaUrl(projectId, exemplarMedia.media_id, 'large')"
-                :style="{
-                  width: getWidth(exemplarMedia),
-                  height: getHeight(exemplarMedia),
-                  maxWidth: '100%',
-                  maxHeight: '300px',
-                  objectFit: 'contain',
-                  backgroundSize: '20px',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundImage: 'url(' + '/images/loader.png' + ')',
-                  backgroundPosition: '10px 10px',
-                }"
-                class="col-md-4 mt-1 ms-0 rounded float-sm-start"
-              />
-            </div>
-            <div class="text-block">
-              <div
-                v-if="exemplarMedia.specimen_name"
-                v-html="'Specimen: ' + exemplarMedia.specimen_name"
-              ></div>
-              <div v-if="exemplarMedia.view_name">
-                View: {{ exemplarMedia.view_name }}
-              </div>
-            </div>
-          </div>
-          <div v-else class="thumbnil-block imgPlaceholderOverview p-2">
-            Project Administrator should add exemplar media
+  <div class="row mt-4">
+    <div class="col-5">
+      <div class="card shadow">
+        <div class="card-header fw-bold">This project contains</div>
+        <div class="card-body">
+          <ul>
+            <li>{{ formatNumber(overview.stats.media) }} Media</li>
+            <li>{{ formatNumber(overview.stats.matrices) }} Matrix</li>
+            <li>{{ formatNumber(overview.stats.docs) }} Documents</li>
+            <li>{{ formatNumber(overview.stats.taxa) }} Taxa</li>
+            <li>{{ formatNumber(overview.stats.specimens) }} Specimens</li>
+            <li>{{ formatNumber(overview.stats.characters) }} Characters</li>
+          </ul>
+          <div>
+            Total size of project's media files:
+            {{ formatBytes(overview.stats.media_size) }}
           </div>
           <div class="mt-3">
-            <router-link to="media"> More Images </router-link>
+            <router-link to="download">
+              <button
+                class="btn btn-primary margin-right"
+                @click="popDownloadAlert()"
+              >
+                Download Project SDD file
+              </button>
+            </router-link>
+            <Tooltip :content="downloadTooltipText"></Tooltip>
           </div>
-        </div>
-
-        <h4>Abstract</h4>
-        <p v-html="overview.description"></p>
-
-        <div class="mt-5" v-if="overview.article_doi">
-          <span class="fw-bold">Article DOI:</span>
-          {{ overview.article_doi }}
-          <a
-            v-if="overview.journal_url"
-            :href="overview.journal_url"
-            target="_blank"
-          >
-            <button class="btn btn-primary btn-ml">Read the article</button>
-          </a>
-        </div>
-
-        <div v-if="overview.project_doi">
-          <span class="fw-bold">Project DOI:</span>
-          {{ overview.project_doi }},
-          <a :href="`http://dx.doi.org/${overview.project_doi}`">
-            http://dx.doi.org/{{ overview.project_doi }}
-          </a>
         </div>
       </div>
     </div>
-
-    <div class="row mt-4">
-      <div class="col-5">
-        <div class="card shadow">
-          <div class="card-header fw-bold">This project contains</div>
-          <div class="card-body">
-            <ul>
-              <li>{{ formatNumber(overview.stats.media) }} Media</li>
-              <li>{{ formatNumber(overview.stats.matrices) }} Matrix</li>
-              <li>{{ formatNumber(overview.stats.docs) }} Documents</li>
-              <li>{{ formatNumber(overview.stats.taxa) }} Taxa</li>
-              <li>{{ formatNumber(overview.stats.specimens) }} Specimens</li>
-              <li>{{ formatNumber(overview.stats.characters) }} Characters</li>
-            </ul>
-            <div>
-              Total size of project's media files:
-              {{ formatBytes(overview.stats.media_size) }}
-            </div>
-            <div class="mt-3">
-              <router-link to="download">
-                <button
-                  class="btn btn-primary margin-right"
-                  @click="popDownloadAlert()"
-                >
-                  Download Project SDD file
-                </button>
-              </router-link>
-              <Tooltip :content="downloadTooltipText"></Tooltip>
-            </div>
-          </div>
+    <div class="col">
+      <div class="card mb-3 shadow">
+        <div class="card-header fw-bold">Matrices</div>
+        <div class="card-body">
+          <ul>
+            <li>
+              Total scored cells:
+              {{ formatNumber(overview.stats.matrix_cells_scored) }}
+            </li>
+            <li>
+              Total media associated with cells:
+              {{ formatNumber(overview.stats.matrix_cell_media) }}
+            </li>
+            <li>
+              Total labels associated with cell media:
+              {{ formatNumber(overview.stats.matrix_cell_media_labels) }}
+            </li>
+          </ul>
         </div>
       </div>
-      <div class="col">
-        <div class="card mb-3 shadow">
-          <div class="card-header fw-bold">Matrices</div>
-          <div class="card-body">
-            <ul>
-              <li>
-                Total scored cells:
-                {{ formatNumber(overview.stats.matrix_cells_scored) }}
-              </li>
-              <li>
-                Total media associated with cells:
-                {{ formatNumber(overview.stats.matrix_cell_media) }}
-              </li>
-              <li>
-                Total labels associated with cell media:
-                {{ formatNumber(overview.stats.matrix_cell_media_labels) }}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="card shadow">
-          <div class="card-header fw-bold">Characters</div>
-          <div class="card-body">
-            <ul>
-              <li>
-                Total characters:
-                {{ formatNumber(overview.stats.character_characters) }}
-              </li>
-              <li>
-                Total characters with associated media:
-                {{ formatNumber(overview.stats.character_media_characters) }}
-              </li>
-              <li>
-                Total characters with media with labels:
-                {{
-                  formatNumber(overview.stats.character_media_characters_labels)
-                }}
-              </li>
-              <li>
-                Total character states:
-                {{ formatNumber(overview.stats.character_states) }}
-              </li>
-              <li>
-                Total character states with associated media:
-                {{ formatNumber(overview.stats.character_state_media) }}
-              </li>
-              <li>
-                Total character states with media with labels:
-                {{ formatNumber(overview.stats.character_state_media_labels) }}
-              </li>
-              <li>
-                Total unordered/ordered characters:
-                {{ formatNumber(overview.stats.character_unordered) }} /
-                {{
-                  formatNumber(
-                    overview.stats.character_characters -
-                      overview.stats.character_unordered
-                  )
-                }}
-              </li>
-            </ul>
-          </div>
+      <div class="card shadow">
+        <div class="card-header fw-bold">Characters</div>
+        <div class="card-body">
+          <ul>
+            <li>
+              Total characters:
+              {{ formatNumber(overview.stats.character_characters) }}
+            </li>
+            <li>
+              Total characters with associated media:
+              {{ formatNumber(overview.stats.character_media_characters) }}
+            </li>
+            <li>
+              Total characters with media with labels:
+              {{
+                formatNumber(overview.stats.character_media_characters_labels)
+              }}
+            </li>
+            <li>
+              Total character states:
+              {{ formatNumber(overview.stats.character_states) }}
+            </li>
+            <li>
+              Total character states with associated media:
+              {{ formatNumber(overview.stats.character_state_media) }}
+            </li>
+            <li>
+              Total character states with media with labels:
+              {{ formatNumber(overview.stats.character_state_media_labels) }}
+            </li>
+            <li>
+              Total unordered/ordered characters:
+              {{ formatNumber(overview.stats.character_unordered) }} /
+              {{
+                formatNumber(
+                  overview.stats.character_characters -
+                    overview.stats.character_unordered
+                )
+              }}
+            </li>
+          </ul>
         </div>
       </div>
     </div>
