3cef0f853c4970b306d46a6f30656ddbb54ee89a
ChenXingguo <xingguochen@alumni.stanford.edu>
2025-08-05 19:08:19 -0700

    MB4-92/93: Add button for EOL/iDigBio import on the taxa list page.

--- src/views/project/taxa/ListView.vue
+++ src/views/project/taxa/ListView.vue
@@ -202,6 +202,26 @@ function clearSearch() {
           <span> Edit Extinct Taxa</span>
         </button>
       </RouterLink>
+      <div class="btn-group">
+        <button
+          type="button"
+          class="btn btn-m btn-outline-primary dropdown-toggle"
+          data-bs-toggle="dropdown"
+        >
+          <i class="fa fa-file-arrow-up"></i>
+          <span> Import Media </span>
+        </button>
+        <div class="dropdown-menu">
+          <RouterLink :to="`/myprojects/${projectId}/media/import/eol`">
+            <button type="button" class="dropdown-item">Import from EOL</button>
+          </RouterLink>
+          <RouterLink :to="`/myprojects/${projectId}/media/import/idigbio`">
+            <button type="button" class="dropdown-item">
+              Import from iDigBio
+            </button>
+          </RouterLink>
+        </div>
+      </div>
     </div>
     <div v-if="taxaStore.taxa?.length">
       <div class="mb-3">
