a689dc140881139b8d8997fd35f04c57c51ded7b
ChenXingguo <xingguochen@alumni.stanford.edu>
2025-09-02 17:32:23 -0700

    Consolidate image size to thumbnail and large.

--- src/lib/matrix-editor/data/Media.ts
+++ src/lib/matrix-editor/data/Media.ts
@@ -4,5 +4,5 @@ export type Media = { url: string; height: number; width: number }
  * Convert a media object to a HTML image tag.
  */
 export function toTag(media: Media): string {
-  return `<img src='${media['url']}' width='${media['width']}' height='${media['height']}' />`
+  return `<img src='${media['url']}' width='${media['width']}' height='${media['height']}' style='max-width: 100%; max-height: 100%; object-fit: contain;' />`
 }
